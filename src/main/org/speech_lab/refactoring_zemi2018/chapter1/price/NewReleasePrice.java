277cf2a7d92efd44b7a72c0fd3d06ab03f2b75e9
wbydo <wbydo@users.noreply.github.com>
2018-09-11 01:08:27 +0900

    PriceのサブクラスにgetChargeの実装を分割(switchの消去)

--- src/main/org/speech_lab/refactoring_zemi2018/chapter1/price/NewReleasePrice.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter1/price/NewReleasePrice.java
@@ -6,4 +6,9 @@ public class NewReleasePrice extends Price {
   public int getPriceCode(){
     return Movie.NEW_RELEASE;
   }
+
+  public double getCharge(int daysRented){
+    return daysRented * 3;
+  }
+
 }
