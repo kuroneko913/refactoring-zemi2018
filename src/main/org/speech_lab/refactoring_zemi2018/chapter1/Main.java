348acf39f9855c7ffe417a2b6ef3c8e815a5d632
wbydo <wbydo@users.noreply.github.com>
2018-09-10 19:16:06 +0900

    chap1: beforeなコード

--- src/main/org/speech_lab/refactoring_zemi2018/chapter1/Main.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter1/Main.java
@@ -2,6 +2,21 @@ package org.speech_lab.refactoring_zemi2018.chapter1;
 
 class Main {
   public static void main(String[] args){
-    System.out.println("Hello World!!!");
+    Movie harawata = new Movie("死霊のはらわた", 0);
+    Movie starwars = new Movie("スター・ウォーズ", 0);
+
+    Movie kametome = new Movie("カメラを止めるな！", 1);
+    Movie kanzou = new Movie("君の肝臓を食べたい", 1);
+
+    Movie shinchan = new Movie("クレヨンしんちゃん", 2);
+    Movie precure = new Movie("プリキュア", 2);
+
+    Customer foo = new Customer("foo");
+    foo.addRental(new Rental(starwars, 1));
+    foo.addRental(new Rental(harawata, 3));
+    foo.addRental(new Rental(kametome, 4));
+    foo.addRental(new Rental(precure, 1));
+
+    System.out.println(foo.statement());
   }
 }
