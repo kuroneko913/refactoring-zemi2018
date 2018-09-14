f7e629f04232e52bd60a64197ba687d3a23980ed
wbydo <wbydo@users.noreply.github.com>
2018-09-14 18:34:59 +0900

    テストコードとして必ず失敗するテストケース追加

---
+++ src/test/org/speech_lab/refactoring_zemi2018/chapter1/CustomerTest.java
@@ -0,0 +1,11 @@
+package org.speech_lab.refactoring_zemi2018.chapter1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CustomerTest {
+  @Test
+  public void statementメソッドが正しい出力を返す(){
+    fail("まだ実装されていません");
+  }
+}
