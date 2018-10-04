1b849435585041dbfbb0fab34e121d82f8e8659b
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2018-10-04 16:54:53 +0900

    Add final indicator in Sales.java

--- src/main/org/speech_lab/refactoring_zemi2018/chapter6/Sales.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter6/Sales.java
@@ -5,24 +5,24 @@ class Sales {
     private int _quantity;
 
     public Sales(int itemPrice, int quantity) {
-	_itemPrice = itemPrice;
-	_quantity = quantity;
+		_itemPrice = itemPrice;
+		_quantity = quantity;
     }
 
     double getPrice() {
-	int basePrice = _quantity * _itemPrice;
-	double discountFactor;
-	if(basePrice > 1000)
-	    discountFactor = 0.95;
-	else
-	    discountFactor = 0.98;
-	return basePrice * discountFactor;
+		final int basePrice = _quantity * _itemPrice;
+		final double discountFactor;
+		if(basePrice > 1000)
+			discountFactor = 0.95;
+		else
+			discountFactor = 0.98;
+		return basePrice * discountFactor;
     }
 
     double price() {
-	// 価格(price)は、基本価格(base price) - 数量割引(quantity) + 送料(shipping)
-	return _quantity * _itemPrice -
-	    Math.max(0, _quantity - 500) * _itemPrice * 0.05 +
-	    Math.min(_quantity * _itemPrice * 0.1, 100.0);
+		// 価格(price)は、基本価格(base price) - 数量割引(quantity) + 送料(shipping)
+		return _quantity * _itemPrice -
+			Math.max(0, _quantity - 500) * _itemPrice * 0.05 +
+			Math.min(_quantity * _itemPrice * 0.1, 100.0);
     }
 }
