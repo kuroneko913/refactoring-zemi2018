cd63b87de6af167f97fade63637a782d1dbfd791
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2018-10-04 16:24:35 +0900

    Fix indent in Customer.java

--- src/main/org/speech_lab/refactoring_zemi2018/chapter6/Customer.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter6/Customer.java
@@ -8,28 +8,28 @@ public class Customer {
     private List<Order> _orders = new ArrayList<Order>();
 
     public Customer(String name) {
-	_name = name;
+		_name = name;
     }
 
     public void addOrder(Order arg) {
-	_orders.add(arg);
+		_orders.add(arg);
     }
 
     void printOwing() {
-	double outstanding = 0.0;
+		double outstanding = 0.0;
 
-	// バナーの印刷
-	System.out.println("**************************");
-	System.out.println("***** Customer Owes ******");
-	System.out.println("**************************");
+		// バナーの印刷
+		System.out.println("**************************");
+		System.out.println("***** Customer Owes ******");
+		System.out.println("**************************");
 
-	// 未払金の計算
-	for(Order each: _orders) {
-	    outstanding += each.getAmount();
-	}
+		// 未払金の計算
+		for(Order each: _orders) {
+			outstanding += each.getAmount();
+		}
 
-	// 明細の印刷
-	System.out.println("name: " + _name);
-	System.out.println("amount: " + outstanding);
+		// 明細の印刷
+		System.out.println("name: " + _name);
+		System.out.println("amount: " + outstanding);
     }
 }
