9fff6f9cc2722f2ee3a5d74234a3d9ad47d81888
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2018-10-11 16:35:23 +0900

    Remove overdraftCharge() from Account class.

--- src/main/org/speech_lab/refactoring_zemi2018/chapter7/Account.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter7/Account.java
@@ -12,14 +12,10 @@ public class Account {
         _interestRate = interestRate;
     }
     
-    double overdraftCharge() {
-        return _type.overdraftCharge(_daysOverdrawn);
-    }
-
     double bankCharge(){
         double result = 4.5;
         if (_daysOverdrawn > 0){
-            result += overdraftCharge();
+            result += _type.overdraftCharge(_daysOverdrawn);
         }
         return result;
     }
