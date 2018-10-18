e3fcbc1b68702a24b6d97e6368046b1fbfdde8f5
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2018-10-18 13:46:00 +0900

    Add setter/getter method in the class Interval.

--- src/main/org/speech_lab/refactoring_zemi2018/chapter8early/Interval.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter8early/Interval.java
@@ -3,4 +3,38 @@ package org.speech_lab.refactoring_zemi2018.chapter8early;
 import java.util.Observable;
 
 class Interval extends Observable {
+
+    private String _start = "0";
+    private String _end = "0";
+    private String _length = "0";
+
+    String getStart() {
+        return _start;
+    }
+
+    void setStart(String arg) {
+        _start = arg;
+        setChanged();
+        notifyObservers();
+    }
+
+    String getEnd() {
+        return _end;
+    }
+
+    void setEnd(String arg) {
+        _end = arg;
+        setChanged();
+        notifyObservers();
+    }
+
+    String getLength() {
+        return _length;
+    }
+
+    void setLength(String arg) {
+        _length = arg;
+        setChanged();
+        notifyObservers();
+    }
 }
