267afbc6e0348460ce4c32dfaa035bbe8be8d28c
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2018-10-18 11:47:51 +0900

    Add a static field in the class Customer.

--- src/main/org/speech_lab/refactoring_zemi2018/chapter8early/Customer.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter8early/Customer.java
@@ -1,8 +1,12 @@
 package org.speech_lab.refactoring_zemi2018.chapter8early;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 class Customer {
 
     private final String _name;
+    private static Dictionary _instances = new Hashtable();
 
     public static Customer create(String name) {
         return new Customer(name);
