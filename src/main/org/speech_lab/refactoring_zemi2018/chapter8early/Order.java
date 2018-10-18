792a07292777e892ac202ee530bd6468ffd9f024
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2018-10-18 11:21:34 +0900

    Replace a String field to a Customer field in the class Order.

--- src/main/org/speech_lab/refactoring_zemi2018/chapter8early/Order.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter8early/Order.java
@@ -1,17 +1,17 @@
 package org.speech_lab.refactoring_zemi2018.chapter8early;
 
 class Order {
-    private String _customer;
+    private Customer _customer;
     
     public Order(String customer) {
-        _customer = customer;
+        _customer = new Customer(customer);
     }
 
     public String getCustomer() {
-        return _customer;
+        return _customer.getName();
     }
 
-    public void setCustomer(String arg) {
-        _customer = arg;
+    public void setCustomer(String customer) {
+        _customer = new Customer(customer);
     }
 }
