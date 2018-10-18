021a9bf63e73b87e0a54fcb72c3c530dedb51a1e
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2018-10-18 12:08:17 +0900

    Replace a String array to String fields in the class Performance.

--- src/main/org/speech_lab/refactoring_zemi2018/chapter8early/Performance.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter8early/Performance.java
@@ -1,21 +1,23 @@
 package org.speech_lab.refactoring_zemi2018.chapter8early;
 
 class Performance {
-    public String[] _data = new String[2];
+    private String[] _data = new String[2];
+    private String _name;
+    private String _wins;
 
     public String getName() {
-        return _data[0];
+        return _name;
     }
 
     public void setName(String arg) {
-        _data[0] = arg;
+        _name = arg;
     }
 
     public int getWins() {
-        return Integer.parseInt(_data[1]);
+        return Integer.parseInt(_wins);
     }
 
     public void setWins(String arg) {
-        _data[1] = arg;
+        _wins = arg;
     }
 }
