ca9b3ff779ad68f1ceadd7081393828e8052c3a4
blackcat913 <mt176240@cc.utsunomiya-u.ac.jp>
2018-11-01 17:29:25 +0900

    getPayAmount (ガード節による条件記述の置き換え)

--- src/main/org/speech_lab/refactoring_zemi2018/chapter9/Pay.java
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter9/Pay.java
@@ -15,16 +15,10 @@ class Pay
     // ---------------------------
     double getPayAmount()
     {
-	double result;
-	if (_isDead) result = deadAmount();
-	else {
-	    if (_isSeparated) result = separatedAmount();
-	    else {
-		if (_isRetired) result = retiredAmount();
-		else result = normalPayAmount();
-	    }
-	}
-	return result;
+	if (_isDead) return deadAmount();
+	if (_isSeparated) return separatedAmount();
+	if (_isRetired) return retiredAmount();
+	return normalPayAmount();
     }
     //-----------------------------
 
