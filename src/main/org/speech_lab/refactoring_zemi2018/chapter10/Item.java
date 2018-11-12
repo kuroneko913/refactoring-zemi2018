67f8e4c81a719e071bbd74858bcf1b73f1b24532
masaki <myblackcat7112@gmail.com>
2018-11-12 14:29:36 +0900

    temp

---
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter10/Item.java
@@ -0,0 +1,7 @@
+package org.speech_lab.refactoring_zemi2018.chapter10;
+
+class Item {
+	private String _itemName;
+	private double _itemPrice;
+
+}
---
+++ src/main/org/speech_lab/refactoring_zemi2018/chapter10/Main.java
@@ -0,0 +1,21 @@
+package org.speech_lab.refactoring_zemi2018.chapter10;
+
+import java.util.List;
+import java.util.ArrayList;
+
+class Main {
+	public static void main(String[] args) {
+		System.out.println("*** Chapter10 ***");
+		/*メソッド名の変更*/
+		/*メソッドのパラメタライズ*/
+		/*明示的なメソッド群によるパラメータの置き換え*/
+		/*オブジェクトそのものの受け渡し*/
+		/*メソッドによるパラメータの置き換え*/
+		
+		/*パラメータオブジェクトの導入*/
+		/*Factory Methodによるコンストラクタの置き換え*/
+		/*ダウンキャストのカプセル化*/
+		/*例外によるエラーコードの置き換え*/
+		/*条件判定による例外の置き換え*/
+	}
+}
